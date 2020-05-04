65f196204625563b5f363b3fcb37066a7170991a
Polina <43153453+ShakhovaP@users.noreply.github.com>
2020-05-04 11:54:31 +0300

    Add files via upload

---
+++ src/mySQL/association.sql
@@ -0,0 +1,4 @@
+INSERT INTO `mydb`.`association` (`id`, `MainForm_id`, `Word_id`, `Tag_id`) VALUES ('1', '1', '1', '41');
+INSERT INTO `mydb`.`association` (`id`, `MainForm_id`, `Word_id`, `Tag_id`) VALUES ('2', '1', '1', '43');
+INSERT INTO `mydb`.`association` (`id`, `MainForm_id`, `Word_id`, `Tag_id`) VALUES ('3', '1', '1', '45');
+INSERT INTO `mydb`.`association` (`id`, `MainForm_id`, `Word_id`, `Tag_id`) VALUES ('4', '1', '1', '44');
